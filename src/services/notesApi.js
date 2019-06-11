7d2f5c5a338d3346e30820760102bbacf07e0dd6
dreamer <wildetangeant@gmail.com>
2019-06-10 21:07:35 -0700

    refactored

--- src/services/notesApi.js
+++ src/services/notesApi.js
@@ -1,19 +1,6 @@
+import { post } from './request';
+
 export const createNote = note => {
-  // return Promise.resolve({
-  //   _id: '123',
-  //   ...note
-  // });
-
-  return fetch('http://localhost:7891/api/v1/notes', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(note)
-  })
-    .then(res => ([res.ok, res.json()]))
-    .then(([ok, json]) => {
-      if(!ok) throw 'unable to fetch';
-      return json;
-    });
-
+  return post('/api/v1/notes', note);
 }
 ;
