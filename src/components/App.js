c54f6d9d95965080cebfbcc83ac77817f07d1d3a
dreamer <wildetangeant@gmail.com>
2019-06-11 12:11:50 -0700

    list works!

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import CreateNote from '../container/notes/createNotes';
+import AllNotes from '../container/notes/AllNotes';
 
 export default function App() {
-  return <CreateNote />;
+  return (
+    <>
+      <CreateNote />
+      <AllNotes />
+    </>
+  );
 
 }
   
